c47421c80d4e226f4618548314dcb4c6a75167f6
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-25 18:57:21 -0300

    conserta maxlength

--- back-end/src/validations/validateProduct.js
+++ back-end/src/validations/validateProduct.js
@@ -1,9 +1,9 @@
 const joi = require('joi');
 
 const schema = joi.object().keys({
-  name: joi.string().empty(false).required().maxlength(100),
+  name: joi.string().empty(false).required().max(100),
   price: joi.number().empty(false).required().min(0),
-  urlImage: joi.string().empty(false).required().maxlength(200),
+  urlImage: joi.string().empty(false).required().max(200),
 });
 
 module.exports = (user) => {
