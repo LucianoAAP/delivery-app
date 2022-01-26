b735177487b5d51dc09f6f990b2e616d99ab454b
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-26 19:24:21 -0300

    REACT: cria app.jsx generico

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+}
+
+export default App;
