23cc4b447ab594fb5c58eafbe98a39759b70c324
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-26 18:42:23 -0300

    REDUX: cria reducer do carrinho

---
+++ front-end/src/redux/reducers/cart.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  cart: [],
+};
+
+const cart = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default cart;
